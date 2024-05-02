0013da55aa75dcb56df5da976676e2fce1999131
Hiba-Aassab <aassab.hiba26@gmail.com>
2024-05-02 10:26:29 +0100

    backend employeeForm code

--- src/MyApp/components/employeeLogin/EmployeeForm.js
+++ src/MyApp/components/employeeLogin/EmployeeForm.js
@@ -1,5 +1,6 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import prismadb from '../../../lib/prismadb';
 // import { useNavigate } from 'react-router-dom';
 
 export default function EmployeeForm() {
@@ -7,6 +8,32 @@ export default function EmployeeForm() {
     // const navigate = useNavigate();
 
     // navigate('/Employee', { replace: true });
+
+    const [email, setEmail]= useState('');
+    const [password, setPassword]= useState('');
+    const [error, setError]= useState('');
+
+    const handleSignIn = async () => {
+        try{
+            const employee = await prismadb.user.findFirst({
+                where: {
+                    email: email,
+                    password: password,
+                    isAdmin: false //khass mykonch admin    
+                }
+            });
+
+            if(employee) {
+
+                console.log('Authentication successful:', employee);
+            }else{
+                setError("Invalid email or password");
+            }
+        }catch(error){
+            console.error("Error authenticating employee", error);
+            setError('An error occurred while authenticating');
+        }
+    };
 
     return (
         <div className='bg-white px-20 py-20 rounded-3xl porder-gray-100'>
@@ -21,6 +48,8 @@ export default function EmployeeForm() {
                         className=' w-full  border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
                         placeholder='xxxxxxxx@eaton.com'
                         type='text'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                      />
                 </div>
                 <div>
@@ -29,14 +58,19 @@ export default function EmployeeForm() {
                         className=' w-full  border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
                         placeholder='enter your password'
                         type='password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                      />
                 </div>
             </div>
             <div className='mt-8 flex flex-col gap-y-4'>
                     <button 
-                        className='active:scale-[.89] hover:scale-[1.01] ease-in-out active:duration-75 transition-all py-3 rounded-xl bg-blue-800 text-white text-lg font-bold'>
+                        className='active:scale-[.89] hover:scale-[1.01] ease-in-out active:duration-75 transition-all py-3 rounded-xl bg-blue-800 text-white text-lg font-bold'
+                        onClick={handleSignIn}
+                        >
                             Sign in
                     </button>
+                    {error && <p className='text-red-500'>{error}</p>}
                     <Link
                         to="/"
                         className='active:scale-[.89] hover:scale-[1.01] ease-in-out active:duration-75 transition-all py-3 hover:text-blue-500 text-lg '
